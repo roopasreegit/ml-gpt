8defc55721680879bdb219ced6158e9e829be2b9
Roopasree <155947971+roopasreegit@users.noreply.github.com>
2026-09-16 11:50:36 +0530

    Implement Character-level encode/decode (Build GPT 2/10)

---
+++ data/vocab.py
@@ -0,0 +1,22 @@
+from typing import Dict, List, Tuple
+
+class Solution:
+    def build_vocab(self, text: str) -> Tuple[Dict[str, int], Dict[int, str]]:
+        # Return (stoi, itos) where:
+        # - stoi maps each unique character to a unique integer (sorted alphabetically)
+        # - itos is the reverse mapping (integer to character)
+        chars=sorted(set(text)) #remove repeating chars, then sort it
+        stoi={ch:i for i,ch in enumerate(chars)} #convert it to hashmap
+        itos={i:ch for i,ch in enumerate(chars)}
+        return stoi, itos
+        pass
+
+    def encode(self, text: str, stoi: Dict[str, int]) -> List[int]:
+        # Convert a string to a list of integers using stoi mapping
+        return [stoi[ch] for ch in text]
+        pass
+
+    def decode(self, ids: List[int], itos: Dict[int, str]) -> str:
+        # Convert a list of integers back to a string using itos mapping
+        return "".join(itos[i] for i in ids)
+        pass
